d85aa9c3ccbcbd8b491d55795e504340c6cac9e1
p4535992 <tentimarco0@gmail.com>
2023-08-15 23:13:49 +0200

    version 0.0.6

--- src/scripts/custom.js
+++ src/scripts/custom.js
@@ -5,8 +5,15 @@ export function setPriceToZeroIfObjectIsNotPreCreatedByGM(doc, createData, optio
   if (!game.user.isGM) {
     const actor = doc.actor;
     log(`setPriceToZeroIfObjectIsNotPreCreatedByGM for item '${createData.name}' on actor '${actor?.name}'`);
-    if (!createData.name || createData.name.includes("(Copy)") || createData.name.startsWith("New ")) {
-      doc.updateSource({ "system.price.value": 0 });
+    if (
+      !createData.name ||
+      createData.name.includes("(Copy)") ||
+      createData.name.includes("(Copia)") ||
+      createData.name.startsWith("New ")
+    ) {
+      doc.updateSource({
+        "system.price.value": 0,
+      });
     }
   }
 }
@@ -15,9 +22,12 @@ export function setPriceToZeroIfObjectIsNotCreatedByGM(item, updates, isDifferen
   if (!game.user.isGM) {
     const actor = item.parent;
     log(`setPriceToZeroIfObjectIsNotCreatedByGM for item '${createData.name}' on actor '${actor?.name}'`);
-    if (!item.name || item.name.includes("(Copy)") || item.name.startsWith("New ")) {
+    if (!item.name || item.name.includes("(Copy)") || item.name.includes("(Copia)") || item.name.startsWith("New ")) {
       if (getProperty(item, "system.price.value") >= 0) {
-        item.update({ "system.price.value": 0 });
+        item.update({
+          "system.price.value": 0,
+          "flags.item-linking.isLinked": null,
+        });
       }
     }
   }
